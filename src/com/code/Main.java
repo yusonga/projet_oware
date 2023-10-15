ee3cd54c23f21d4e65910a9a95e1197ca4c65e1d
YuSONG <songyufivo@gmail.com>
2023-10-15 22:24:36 +0200

    modification de makemove

--- src/com/code/Main.java
+++ src/com/code/Main.java
@@ -1,10 +1,12 @@
 package com.code;
 
+import java.util.Random;
+
 class Hole {
-    private int number;
-    private int redSeeds;
-    private int blueSeeds;
-    private int transparentSeeds;
+    public int number;
+    public int redSeeds;
+    public int blueSeeds;
+    public int transparentSeeds;
 
     public Hole(int number, int redSeeds, int blueSeeds, int transparentSeeds) {
         this.number = number;
@@ -71,30 +73,91 @@ class OwareBoard {
     }
     public void initializeBoard() {
         // Initialize holes with numbers and seed counts for each color
-        for (int i = 0; i < 8; i++) {
+		/*
+		 * for (int i = 0; i < 8; i++) { holes[i] = new Hole(i + 1, 2, 2, 1); // 2 Red,
+		 * 2 Blue, and 1 Transparent holes[i + 8] = new Hole(16 - i, 2, 2, 1); // 2 Red,
+		 * 2 Blue, and 1 Transparent }
+		 */
+        for (int i = 0; i < 16; i++) {
             holes[i] = new Hole(i + 1, 2, 2, 1); // 2 Red, 2 Blue, and 1 Transparent
-            holes[i + 8] = new Hole(16 - i, 2, 2, 1); // 2 Red, 2 Blue, and 1 Transparent
-        }
-        holes[4]=new Hole(5,0,0,0);
-        holes[5]=new Hole(6,0,0,1);
-        holes[6]=new Hole(7,0,1,1);
-        holes[7]=new Hole(8,1,0,0);
-        holes[1]=new Hole(2,1,0,1);
-        holes[2]=new Hole(3,1,1,0);
-        holes[3]=new Hole(4,0,1,0);
+             }
+        
 
     }
 
     public void displayBoard() {
         // Print the board using the Hole objects
-        for (int i = 0; i < 16; i++) {
-            System.out.print(holes[i]);
-            if ((i + 1) % 8 == 0) {
-                System.out.println(); // New line after every 8 holes
-            } else {
-                System.out.print("  ");
-            }
+        for (int i = 0; i<8; i++) {
+           
+            	System.out.print(holes[i]);
+            	System.out.print("  ");
         }
+        System.out.println();
+        for (int i = 15; i>=8; i--) {
+            
+        	System.out.print(holes[i]);
+        	System.out.print("  ");
+        }
+        System.out.println();
+    }
+    
+    public void makeMove(int holeNumber, char seedColor) {
+    	
+    	if (holeNumber % 2 == 0) {
+    		Hole chosenHole = holes[holeNumber - 1];
+            int seedsToDistribute;
+            char distributionBehavior = seedColor; // Default to seedColor
+
+            switch (seedColor) {
+                case 'R':
+                    seedsToDistribute = chosenHole.getRedSeeds();
+                    chosenHole.redSeeds = 0;
+                    break;
+                case 'B':
+                    seedsToDistribute = chosenHole.getBlueSeeds();
+                    chosenHole.blueSeeds = 0;
+                    break;
+                case 'T':
+                    seedsToDistribute = chosenHole.getTransparentSeeds();
+                    chosenHole.transparentSeeds = 0;
+                    Random rand = new Random();
+                    distributionBehavior = rand.nextBoolean() ? 'R' : 'B'; // Randomly choose to distribute like red or blue
+                    break;
+                default:
+                    throw new IllegalArgumentException("Invalid seed color!");
+            }
+
+            int currentHole = holeNumber;
+            while (seedsToDistribute > 0) {
+            	// Move clockwise
+            	
+            	currentHole = (currentHole % 16) + 1;
+            	Hole hole = holes[currentHole - 1];
+                
+
+
+                if (distributionBehavior == 'B' && (currentHole % 2 == 0)) {
+                    continue; // Skip player's own holes when distributing like blue seeds
+                }
+
+                // Place the actual seed (which is seedColor) in the hole
+                switch (seedColor) {
+                    case 'R':
+                        hole.redSeeds++;
+                        break;
+                    case 'B':
+                        hole.blueSeeds++;
+                        break;
+                    case 'T':
+                        hole.transparentSeeds++;
+                        break;
+                }
+                
+                seedsToDistribute--;
+            }
+	    } else {
+	        throw new IllegalArgumentException("Second player can only choose even holes!");
+	    }
     }
 	
 	
@@ -102,15 +165,43 @@ class OwareBoard {
 
 
 
-class Game{
-	
-    
+class Game {
+    private OwareBoard board;
+
+    public Game() {
+        this.board = new OwareBoard();
+    }
+
+    public void playMove() {
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        System.out.print("Player even plays ");
+        String input = scanner.next();
+
+        int holeChoice = Character.getNumericValue(input.charAt(0)); // Extract the hole number
+        char seedColor = input.charAt(1); // Extract the seed color
+
+        board.makeMove(holeChoice, seedColor);
+
+		/*
+		 * String colorPlayed; switch (seedColor) { case 'R': colorPlayed = "Red";
+		 * break; case 'B': colorPlayed = "Blue"; break; case 'T': colorPlayed =
+		 * "Transparent"; break; default: colorPlayed = ""; }
+		 */
+
+        System.out.println("The sowing leads to  ");
+        board.displayBoard();
+        scanner.close();
+    }
 }
 
 public class Main {
 	public static void main(String[] args) {
-		OwareBoard owareBoard = new OwareBoard();
-        owareBoard.displayBoard();
+		//OwareBoard owareBoard = new OwareBoard();
+		//System.out.print("The result is: \n");
+        //owareBoard.displayBoard();
+        Game game = new Game();
+        //System.out.println("Initial Board:");
+        game.playMove();
         
     }
 }
